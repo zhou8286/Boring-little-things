8a89a442bc27c2b9ed9f92e1c1e155853451df25
zhou8286 <160570594+zhou8286@users.noreply.github.com>
2024-02-22 14:37:54 +0800

    Boring copying.cpp
    hello!This is a boring thing, whatever you write, it outputs!
    你好！ 这是一个无聊的东西，你写入什么，他就输出什么！

---
+++ Boring copying.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  char a；
+  cout<<"hello!This is a boring thing, whatever you write, it outputs!"<<endl；
+  cout<<"你好！ 这是一个无聊的东西，你写入什么，他就输出什么！"<<endl；
+  for(int i=0；；i++){
+    cout<<""ENTER/请输入：";
+    cin>>a；
+    cout<<a<<endl；
+  }
+  return 0;
+}
